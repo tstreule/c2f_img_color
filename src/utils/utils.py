8e5f90fcf40f32d567a6146e76238ccc7cc00c02
Timo Streule <timo.streule@bluewin.ch>
2021-12-17 11:41:25 +0100

    Rename folder  to  to prevent PyCharm errors

---
+++ src/utils/utils.py
@@ -0,0 +1,48 @@
+from PIL import Image
+import numpy as np
+from skimage.color import rgb2lab, lab2rgb
+
+import torch
+from torch import nn, optim
+from torchvision import transforms
+from torchvision.utils import make_grid
+from torch.utils.data import Dataset, DataLoader
+
+
+SIZE = 256
+
+
+class ColorizationDataset(Dataset):
+    def __init__(self, paths, split='train'):
+        if split == 'train':
+            self.transforms = transforms.Compose([
+                transforms.Resize((SIZE, SIZE), Image.BICUBIC),
+                transforms.RandomHorizontalFlip(),  # a little data augmentation!
+            ])
+        elif split == 'val':
+            self.transforms = transforms.Resize((SIZE, SIZE), Image.BICUBIC)
+
+        self.split = split
+        self.size = SIZE
+        self.paths = paths
+
+    def __getitem__(self, idx):
+        img = Image.open(self.paths[idx]).convert("RGB")
+        img = self.transforms(img)
+        img = np.array(img)
+        img_lab = rgb2lab(img).astype("float32")  # converting RGB to L*a*b
+        img_lab = transforms.ToTensor()(img_lab)
+        L = img_lab[[0], ...] / 50.0 - 1.0  # between -1 and 1
+        ab = img_lab[[1, 2], ...] / 110.0  # between -1 and 1
+
+        return {'L': L, 'ab': ab}
+
+    def __len__(self):
+        return len(self.paths)
+
+
+def make_dataloaders(batch_size=16, n_workers=4, pin_memory=True, **kwargs):  # a handy function to make our dataloaders
+    dataset = ColorizationDataset(**kwargs)
+    dataloader = DataLoader(dataset, batch_size=batch_size, num_workers=n_workers,
+                            pin_memory=pin_memory)
+    return dataloader
